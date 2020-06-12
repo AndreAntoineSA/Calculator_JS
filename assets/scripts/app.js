eaf4c675ffe826afb23b9fa1dc2dc021920604e3
andre <andreantoine0016@gmail.com>
2020-06-12 22:20:51 +0200

    final commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,60 +2,60 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
-function getUserInput(){
+function getUserInput() {
     return parseInt(usrInput.value);
 }
 
-function writeOutput(operator,beforeCalc,aftercalc){
+function writeOutput(operator, beforeCalc, aftercalc) {
     const calcDescription = `${beforeCalc} ${operator} ${aftercalc}`;
-    outputResult(currentResult,calcDescription);
+    outputResult(currentResult, calcDescription);
 }
-function writeLog(operate , entry1, entry2, calc){
-    const logEntry ={
-        op : operate,
-        num1 : entry1,
-        num2 : entry2,
-        result : calc,
+function writeLog(operate, entry1, entry2, calc) {
+    const logEntry = {
+        op: operate,
+        num1: entry1,
+        num2: entry2,
+        result: calc,
     };                      //object creation 
     console.log(logEntry.op);
     logEntries.push(logEntry);
     console.log(logEntries);
 }
-function add(){
+function add() {
     const enteredNumber = getUserInput();
     const secondNumber = currentResult;
     currentResult += enteredNumber;
-    writeOutput('+',secondNumber,enteredNumber);
-    writeLog('ADD',secondNumber,enteredNumber,currentResult);
-    
-    
+    writeOutput('+', secondNumber, enteredNumber);
+    writeLog('ADD', secondNumber, enteredNumber, currentResult);
+
+
 }
-function sub(){
+function sub() {
     const enteredNumber = getUserInput();
     const secondNumber = currentResult;
     currentResult -= enteredNumber;
-    writeOutput('-',secondNumber,enteredNumber);
-    writeLog('SUB',secondNumber,enteredNumber,currentResult);
+    writeOutput('-', secondNumber, enteredNumber);
+    writeLog('SUB', secondNumber, enteredNumber, currentResult);
 
 }
 
-function mul(){
+function mul() {
     const enteredNumber = getUserInput();
     const secondNumber = currentResult;
     currentResult *= enteredNumber;
-    writeOutput('*',secondNumber,enteredNumber);
-    writeLog('MULTIPLY',secondNumber,enteredNumber,currentResult);
+    writeOutput('*', secondNumber, enteredNumber);
+    writeLog('MULTIPLY', secondNumber, enteredNumber, currentResult);
 }
 
-function div(){
+function div() {
     const enteredNumber = getUserInput();
     const secondNumber = currentResult;
     currentResult /= enteredNumber;
-    writeOutput('/',secondNumber,enteredNumber);
-    writeLog('DIVIDE',secondNumber,enteredNumber,currentResult);
+    writeOutput('/', secondNumber, enteredNumber);
+    writeLog('DIVIDE', secondNumber, enteredNumber, currentResult);
 }
 
-addBtn.addEventListener('click',add);
-subtractBtn.addEventListener('click',sub);
-multiplyBtn.addEventListener('click',mul);
-divideBtn.addEventListener('click',div);
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', sub);
+multiplyBtn.addEventListener('click', mul);
+divideBtn.addEventListener('click', div);
