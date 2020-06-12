334f217558881b433702aef15d053765ee62907f
andre <andreantoine0016@gmail.com>
2020-06-12 20:16:25 +0200

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,44 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserInput(){
+    return parseInt(usrInput.value);
+}
+
+function writeOutput(operator,beforeCalc,aftercalc){
+    const calcDescription = `${beforeCalc} ${operator} ${aftercalc}`;
+    outputResult(currentResult,calcDescription);
+}
+function add(){
+    const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
+    currentResult = currentResult + enteredNumber;
+    writeOutput('+',secondNumber,enteredNumber)
+    
+}
+function sub(){
+    const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
+    currentResult = currentResult - enteredNumber;
+    writeOutput('-',secondNumber,enteredNumber)
+
+}
+
+function mul(){
+    const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
+    currentResult = currentResult * enteredNumber;
+    writeOutput('*',secondNumber,enteredNumber)
+}
+
+function div(){
+    const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
+    currentResult = currentResult / enteredNumber;
+    writeOutput('/',secondNumber,enteredNumber)
+}
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',sub);
+multiplyBtn.addEventListener('click',mul);
+divideBtn.addEventListener('click',div);
